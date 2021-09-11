1d638eb415dcefb54ac9513d26eae673efc5c7f9
Yohei Shiina <ysys.j.0125@gmail.com>
2021-09-11 15:56:59 +0900

    add select and infobox

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,64 +1,67 @@
-import React, { useState, useEffect, useRef } from "react";
-import {
-  MenuItem,
-  FormControl,
-  Select,
-} from "@material-ui/core";
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { MenuItem, FormControl, Select } from "@material-ui/core";
+import "./App.css";
+import InfoBox from './InfoBox'
+import Map from './Map'
 
 function App() {
-  const divRef = useRef(null);
-  const [countries, setCountries] = useState([]);
+	const [countries, setCountries] = useState([]);
+	const [country, setCountry] = useState("Worldwide");
 
-  useEffect(()=>{
-    const getCountriesData = async () => {
-      await fetch("https://disease.sh/v3/covid-19/countries")
-        .then((res) => res.json())
-        .then((data) => {
-          const countries = data.map((country) => {
-            return {
-              name: country.country,
-              value: country.countryInfo.iso2,
-            }
-          })
-          console.log("countries")
-          console.log(countries)
-          setCountries(countries)
-      }, [])
-    }
+	useEffect(() => {
+		const getCountriesData = async () => {
+			const res = await fetch("https://disease.sh/v3/covid-19/countries");
+			const data = await res.json();
 
-    getCountriesData();
-  }, [])
-  // STATE = How to write a variable in REACT
-  return (
-    <div className="app">
-      <div className="app__header">
-      <h1>COVID-19 TRACKER</h1>
-      <FormControl className="app__dropdown">
-        <Select variant="outlined" value="">
-          {countries.map((country, index)=>{
-            return (
-              <MenuItem key={index} ref={divRef} value={country.value} >{country.name}</MenuItem>
-            )
-          })}
-        </Select>
-      </FormControl>
-      </div>
-      {/* {console.log(divRef)} */}
-      
-      {/* Header */}
-      {/* Title + Select input dropdown field */}
+			const countries = data.map((country) => {
+				return {
+					name: country.country,
+					value: country.countryInfo.iso2,
+				};
+			});
 
-      {/* InfoBoxes */}
-      {/* InfoBoxes */}
-      {/* InfoBoxes */}
+			setCountries(countries);
+		};
 
-      {/* Table */}
-      {/* Graph */}
+		getCountriesData();
+	}, []);
 
-      {/* Map */}
-    </div>
-  );
+	const onCountryChange = async (event) => {
+		const countryCode = event.target.value;
+		setCountry(countryCode);
+	}
+
+	// STATE = How to write a variable in REACT
+	return (
+		<div className="app">
+			<div className="app__header">
+				<h1>COVID-19 TRACKER</h1>
+				<FormControl className="app__dropdown">
+					<Select variant="outlined" onChange={onCountryChange} value={country}>
+						<MenuItem value="Worldwide">Worldwide</MenuItem>
+						{countries.map((country, index) => {
+							return (
+								<MenuItem key={index} value={country.value}>{country.name}</MenuItem>
+							);
+						})}
+					</Select>
+				</FormControl>
+			</div>
+
+			<div className="app__stats">
+				<InfoBox title="Coronavirus Cases" cases="+2,500" total="1.2 M Total" />
+				<InfoBox title="Recovered" cases="+2,500" total="1.2 M Total" />
+				<InfoBox title="Death" cases="+2,500" total="1.2 M Total" />
+			</div>
+
+
+			{/* Table */}
+			{/* Graph */}
+
+			{/* Map */}
+			<Map />
+		</div>
+	);
 }
 
 export default App;
---
+++ react-app/src/InfoBox.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Card, CardContent, Typography } from '@material-ui/core'
+
+function InfoBox({ title, cases, total }) {
+  return (
+    <div>
+      <Card className="infoBox">
+        <CardContent>
+          {/* title */}
+          <Typography lassName="infoBox__title" color="textSecondary">{title}</Typography>
+          {/* +120k Number of cases */}
+          <h2 className="infoBox__cases">{cases}</h2>
+          {/* 1.2M Total */}
+          <Typography className="infoBox__total" color="textSecondary">
+            {total} Total
+          </Typography>
+        </CardContent>
+      </Card>
+    </div>
+  )
+}
+
+export default InfoBox
